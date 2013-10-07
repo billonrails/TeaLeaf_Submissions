79e9e1b37d656834ccd45cfb645d9566d0834162
Bill Lindeman <bill.lindeman@icloud.com>
2013-10-06 23:08:57 -0700

    first_stab att oop blackjack

--- blackjack_oop.rb
+++ blackjack_oop.rb
@@ -1,27 +1,58 @@
 class Deck
 
-  attr_accessor :suit, :cards
-
-  def initialize(s, c)
-    @suit = ['hearts', 'diamonds', 'spades', 'clubs'
+  #attr_accessor :suit, :cards
+  def initialize 
+    @suit = ['hearts', 'diamonds', 'spades', 'clubs']
     @cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-  end
-  # combines the suit and cards to make a full deck in one array
-  def create_deck(:suit, :cards)
-    @deck = suit.product(cards)
+    @deck = @suit.product(@cards)
+    #@deck.each { |x| puts x}
   end
 
-  def shuffle(@deck)
-    @deck.shuffle!
+  def shuffle
+   @deck = @deck.shuffle!
   end 
 end
 
-class Card
 
-end 
+class Cards
+  def initialize
+    @player_cards = []
+    @dealer_cards = []
+  end
+
+  def first_hand
+    @player_cards << @deck.pop
+    @dealer_cards << @deck.pop
+    @player_cards << @deck.pop
+    @dealer_cards << @deck.pop
+  end 
+
+  def calculate_total(cards)
+  #['H', '3'], ['J', 'Q']
+    arr = cards.map { |e| e[1] }
+    total = 0
+    arr.each do |value|
+      if value == "A"
+        total += 11
+      elsif value.to_i == 0
+        total = total + 10
+      else 
+        total += value.to_i
+      end
+  end
+
+  #correct for Aces
+  arr.select {|e| e == "A"}.count.times do
+      total -= 10 if total > 21
+  end
+
+  total #last value implicitly returned in the method
+end
 
 class Player
-
+  def initialize 
+    @name = name
+  end
 end
 
 class Dealer
@@ -31,6 +62,5 @@ end
 class Blackjack
 
 end 
-
 
 
