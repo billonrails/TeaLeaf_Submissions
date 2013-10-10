3e62173a0c2ac216e4005fa23114cf14e7be10bd
Bill Lindeman <bill.lindeman@icloud.com>
2013-10-10 12:32:56 -0700

    Latest Develops in the struggle for OOP Blackjack

--- blackjack_oop.rb
+++ blackjack_oop.rb
@@ -1,34 +1,106 @@
-class Deck
+require 'rubygems'
+require 'pry' 
 
-  #attr_accessor :suit, :cards
+class Deck
+  attr_accessor :cards
+
   def initialize 
-    @suit = ['hearts', 'diamonds', 'spades', 'clubs']
-    @cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-    @deck = @suit.product(@cards)
-    #@deck.each { |x| puts x}
+    @cards = []
+    ['H', 'D', 'S', 'C'].each do |suit|
+      ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'].each do |face_value|
+        @cards << Card.new(suit, face_value)
+      end
+    end 
+    scramble!
   end
 
-  def shuffle
-   @deck = @deck.shuffle!
+  def scramble!
+    cards.shuffle!
+  end
+
+  def deal_one
+    cards.pop
+  end
+
+  def deal_two
+    2.times do
+      cards.pop
+    end
+  end
+
+  def size
+    cards.size
   end 
+
 end
 
 
-class Cards
-  def initialize
-    @player_cards = []
-    @dealer_cards = []
+class Card
+  attr_accessor :suit, :face_value
+
+  def initialize(s, fv)
+    @suit = s
+    @face_value = fv
   end
 
-  def first_hand
-    @player_cards << @deck.pop
-    @dealer_cards << @deck.pop
-    @player_cards << @deck.pop
-    @dealer_cards << @deck.pop
+  def pretty_output
+    puts "The #{face_value} of #{find_suit}"
+  end
+
+  def to_s
+    pretty_output
+  end
+
+  def find_suit
+    case suit
+      when 'H' then 'Hearts'
+      when 'D' then 'Diamonds'
+      when 'S' then 'Spades'
+      when 'C' then 'Clubs'
+    end
+  end
+end
+
+# hold players cards  
+class Player
+  attr_accessor :name, :cards
+  include Calculate
+  def initialize(n)
+    @name = n
+    puts "Welcome to the game #{name}"
+    @player_cards = Hand.new
+  end
+
+end
+# hold dealers cards
+class Dealer 
+  attr_accessor :cards
+  include Calculate
+  def initialize
+    @dealer_cards = []
+    @dealer_cards << Hand.new
+  end
+
+end 
+# calculate the hand
+class Hand 
+  attr_accessor :cards
+
+  def initialize
+    @cards = []
+    @cards << Deck.deal_two
   end 
 
-  def calculate_total(cards)
-  #['H', '3'], ['J', 'Q']
+  def hit
+    Deck.deal_one
+  end
+
+  def stay
+    puts "You chose to stay"
+  end
+end
+
+module Calculate
     arr = cards.map { |e| e[1] }
     total = 0
     arr.each do |value|
@@ -39,28 +111,23 @@ class Cards
       else 
         total += value.to_i
       end
-  end
+    end
 
-  #correct for Aces
-  arr.select {|e| e == "A"}.count.times do
-      total -= 10 if total > 21
-  end
+    #correct for Aces
+    arr.select {|e| e == "A"}.count.times do
+        total -= 10 if total > 21
+    end
 
-  total #last value implicitly returned in the method
-end
-
-class Player
-  def initialize 
-    @name = name
+    total #last value implicitly returned in the method
   end
 end
 
-class Dealer
 
-end 
 
-class Blackjack
+deck = Deck.new
+player1 = Player.new("Bill")
 
-end 
+#puts deck.cards
+
 
 
